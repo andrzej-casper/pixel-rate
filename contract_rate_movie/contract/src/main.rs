cd4e0a6e34a5be4e0d372978539692eef58d991e
Andrzej Broński <andrzej@casper.network>
2023-05-12 11:54:25 +0200

    Make rate_movie session code deployed as entrypoint

--- contract_rate_movie/contract/src/main.rs
+++ contract_rate_movie/contract/src/main.rs
@@ -6,20 +6,23 @@ compile_error!("target arch should be wasm32: compile with '--target wasm32-unkn
 
 extern crate alloc;
 
-use alloc::string::String;
+use alloc::{string::String, vec::Vec};
 
 use casper_contract::{
     contract_api::{runtime, storage},
     unwrap_or_revert::UnwrapOrRevert,
 };
-use casper_types::URef;
+use casper_types::{URef, EntryPoint, EntryPoints, EntryPointAccess, EntryPointType, CLType};
 use core::convert::TryInto;
 
 const MOVIE_ARG_NAME: &str = "movie";
 const RATING_ARG_NAME: &str = "rating";
 
+const ENTRY_POINT_RATE_MOVIE: &str = "rate_movie";
+
+// Entry point that stores movie rating in named key.
 #[no_mangle]
-pub extern "C" fn call() {
+pub extern "C" fn rate_movie() {
     let movie: String = runtime::get_named_arg(MOVIE_ARG_NAME);
     let rating: String = runtime::get_named_arg(RATING_ARG_NAME);
 
@@ -34,4 +37,23 @@ pub extern "C" fn call() {
             storage::write(key, rating);
         }
     }
+}
+
+#[no_mangle]
+pub extern "C" fn call() {
+    // Define entrypoints - one session code for rating movie.
+    let mut entry_points = EntryPoints::new();
+    entry_points.add_entry_point(EntryPoint::new(
+        ENTRY_POINT_RATE_MOVIE,
+        Vec::new(), // just info???
+        CLType::Unit,
+        EntryPointAccess::Public,
+        EntryPointType::Session,
+    ));
+
+    // Install as non-upgradable contract.
+    let (contract_hash, _) = storage::new_locked_contract(entry_points, None, None, None);
+
+    // Store a named key with the contract hash (under current account).
+    runtime::put_key("contract_hash", contract_hash.into());
 }
