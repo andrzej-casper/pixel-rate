a9057b55d4df4805fdfb1b78a82f0b9c05337c89
Andrzej Broński <andrzej@casper.network>
2023-05-16 13:25:32 +0200

    Update README with local setup guide

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,14 +5,14 @@ import movies  from "./movies.json";
 const prisma = new PrismaClient();
 
 async function seed() {
-  const email = "rachel@remix.run";
+  const email = "test@casper.network";
 
   // cleanup the existing database
   await prisma.user.delete({ where: { email } }).catch(() => {
     // no worries if it doesn't exist yet
   });
 
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+  const hashedPassword = await bcrypt.hash("testtest", 10);
 
   const user = await prisma.user.create({
     data: {
