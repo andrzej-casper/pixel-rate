571d5938d44c2eb706195fd6670b53af66080a61
Andrzej Broński <andrzej@casper.network>
2023-05-16 12:57:46 +0200

    Fix config file - add missing export

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
 //const NODE_URL = "http://65.108.127.242:7777/rpc";
-const NODE_URL = "http://casper-node.xyz:7777/rpc/";
+export const NODE_URL = "http://casper-node.xyz:7777/rpc/";
 
 // Deployment details taken from https://github.com/andrzej-casper/pixel-rate-contract/README.adoc
-const NETWORK_NAME = "casper-test";
-const CONTRACT_HASH = "hash-be3d58d00bfe1af1219addce925af80d68ca6bbfad3d00f36ed9cefa2ecc4266";
+export const NETWORK_NAME = "casper-test";
+export const CONTRACT_HASH = "hash-be3d58d00bfe1af1219addce925af80d68ca6bbfad3d00f36ed9cefa2ecc4266";
